2eee9b67bc470543ae5abc185b7d520ebec1db29
nakamura0907 <nakamura990907@gmail.com>
2023-10-06 10:41:43 +0900

    アプリ形式の決定

---
+++ src/bin/git-chj.rs
@@ -0,0 +1,6 @@
+fn main() {
+    match git_commit_helper_jp::run() {
+        Ok(_) => {}
+        Err(_e) => {}
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+use std::io::Result;
+
+pub fn run() -> Result<()> {
+    println!("run cli tool!");
+
+    Ok(())
+}
