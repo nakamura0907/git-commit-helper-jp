96a9d5ee3c8ee0504bb89f455e15b2b43bf4afe7
nakamura0907 <nakamura990907@gmail.com>
2023-10-08 23:01:17 +0900

    clapの導入

--- src/bin/git-chjp.rs
+++ src/bin/git-chjp.rs
@@ -1,6 +1,14 @@
+use clap::Parser;
 use std::process;
 
+#[derive(Parser)]
+#[clap(name = "git", bin_name = "git chjp")]
+#[command(version, about, long_about = None)]
+struct Args {}
+
 fn main() {
+    Args::parse();
+
     if let Err(e) = git_commit_helper_jp::run() {
         eprintln!("{}", e);
         process::exit(1);
